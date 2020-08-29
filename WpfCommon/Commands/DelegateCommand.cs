8d0c39d380ead364d2350d58e31ffb5c5a403b0a
m-bergstrom <mbergstrom@acm.org>
2020-08-29 18:43:52 -0400

    Initial commit

---
+++ WpfCommon/Commands/DelegateCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+using JetBrains.Annotations;
+
+namespace WpfCommon.Commands
+{
+    public class DelegateCommand : ICommand
+    {
+        [NotNull] private readonly Action<object> _Execute;
+        [NotNull] private readonly Func<object, bool> _CanExecute;
+
+        public DelegateCommand([NotNull] Action<object> execute, [NotNull] Func<object, bool> canExecute)
+        {
+            _Execute = execute;
+            _CanExecute = canExecute;
+        }
+
+        public DelegateCommand([NotNull] Action execute, [NotNull] Func<bool> canExecute) : this(o => execute(),
+            o => canExecute())
+        {
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _CanExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _Execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
